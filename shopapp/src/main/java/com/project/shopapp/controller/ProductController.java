65fcaadd64912aaf0ae7bf6cc09f1e2ee366c0e5
Nguyễn Thái Vinh <VinhNT@gmail.com>
2024-08-18 23:18:39 +0700

    add product functions
    created, upload, delete, upload images, save file images, view image, valid

--- shopapp/src/main/java/com/project/shopapp/controller/ProductController.java
+++ shopapp/src/main/java/com/project/shopapp/controller/ProductController.java
@@ -1,17 +1,27 @@
 package com.project.shopapp.controller;
-
-
-import com.project.shopapp.dto.ProductDTO;
-import jakarta.validation.Valid;
-
+import com.project.shopapp.dto.*;
+import com.project.shopapp.model.Product;
+import com.project.shopapp.model.ProductImage;
+import com.project.shopapp.responses.ProductListResponse;
+import com.project.shopapp.responses.ProductResponse;
+import com.project.shopapp.services.interfaces.IProductService;
+import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.UrlResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import jakarta.validation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,62 +33,99 @@ import java.util.Objects;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("api/v1/products")
+@RequestMapping("${api.prefix}/products") // http://localhost:8088/v1/api/products
+@RequiredArgsConstructor
 public class ProductController {
-    //    http://localhost:8088/api/v1/products
-/*
-{
-    "name" : "vinh",
-    "price" : 1000,
-    "thumbnail": "",
-    "description" :"",
-    "category_id" : "1"
-}
- */
-    @GetMapping("")
-    public ResponseEntity<?> getAllProducts() {
-        return ResponseEntity.ok("show all ok");
-    }
+    private final IProductService productService;
 
-    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> insertProduct(
-            @Valid @ModelAttribute ProductDTO productDTO,
+    @PostMapping("")
+    public ResponseEntity<?> createProduct(
+            @Valid @RequestBody ProductDTO productDTO,
             BindingResult result
     ) {
-        // trả về mess báo lỗi
         try {
-            if (result.hasErrors()) {
+            if(result.hasErrors()) {
                 List<String> errorMessages = result.getFieldErrors()
                         .stream()
                         .map(FieldError::getDefaultMessage)
                         .toList();
                 return ResponseEntity.badRequest().body(errorMessages);
             }
-            // kiểm tra file
-            List<MultipartFile> files = productDTO.getFile();
-            files = files == null ? new ArrayList<>() : files; // nếu null thi trả về list rỗng cho lúc xet contentType bắt ngoại lệ
-            for (MultipartFile file : files) {
-                String contentType = file.getContentType();
-                if (contentType == null || !contentType.startsWith("image/"))
-                    return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body("not found or not support");
-                String fileName = storeFile(file);
-            }
-            return ResponseEntity.ok(" upload ok");
+            Product newProduct = productService.createProduct(productDTO);
+            return ResponseEntity.ok(newProduct);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
-
     }
+        @PostMapping(value = "uploads/{id}",
+                consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//    POST http://localhost:8088/v1/api/products/uploads/1
+    public ResponseEntity<?> uploadImages(
+            @PathVariable("id") Long productId,
+            @ModelAttribute("files") List<MultipartFile> files
+    ) throws Exception{
+        try {
+            Product existingProduct = productService.getProductById(productId);
+            files = files == null ? new ArrayList<MultipartFile>() : files;
+            if(files.size() > ProductImage.MAXIMUM_IMAGES_PER_PRODUCT) {
+                return ResponseEntity.badRequest().body("max = 5");
+            }
+            List<ProductImage> productImages = new ArrayList<>();
+            for (MultipartFile file : files) {
+                if(file.getSize() == 0) {
+                    continue;
+                }
+//                if(file.getSize() > 10 * 1024 * 1024) {
+//                    return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+//                            .body("max size = 10mb");
+//                }
+                String contentType = file.getContentType();
+                if(contentType == null || !contentType.startsWith("image/")) {
+                    return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                            .body("is not image");
+                }
+                // Lưu file và cập nhật thumbnail trong DTO
+                String filename = storeFile(file);
+                //lưu vào đối tượng product trong DB
+                ProductImage productImage = productService.createProductImage(
+                        existingProduct.getId(),
+                        ProductImageDTO.builder()
+                                .imageUrl(filename)
+                                .build()
+                );
+                productImages.add(productImage);
+            }
+            return ResponseEntity.ok().body(productImages);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @GetMapping("/images/{imageName}") // ?
+    public ResponseEntity<?> viewImage(@PathVariable String imageName) {
+        try {
+            java.nio.file.Path imagePath = Paths.get("images/products/"+imageName);
+            UrlResource resource = new UrlResource(imagePath.toUri());
 
+            if (resource.exists()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG)
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
     private String storeFile(MultipartFile file) throws IOException {
-        if (file.getOriginalFilename() == null) {
+        if (!isImageFile(file) || file.getOriginalFilename() == null) {
             throw new IOException("Invalid image format");
         }
         String filename = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
         // Thêm UUID vào trước tên file để đảm bảo tên file là duy nhất
         String uniqueFilename = UUID.randomUUID().toString() + "_" + filename;
         // Đường dẫn đến thư mục mà bạn muốn lưu file
-        java.nio.file.Path uploadDir = Paths.get("images/products"); //shopapp/uploads
+        java.nio.file.Path uploadDir = Paths.get("uploads");
         // Kiểm tra và tạo thư mục nếu nó không tồn tại
         if (!Files.exists(uploadDir)) {
             Files.createDirectories(uploadDir);
@@ -89,14 +136,61 @@ public class ProductController {
         Files.copy(file.getInputStream(), destination, StandardCopyOption.REPLACE_EXISTING);
         return uniqueFilename;
     }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<String> updateProduct(@PathVariable Long id) {
-        return ResponseEntity.ok("updated " + id);
+    private boolean isImageFile(MultipartFile file) {
+        String contentType = file.getContentType();
+        return contentType != null && contentType.startsWith("image/");
     }
+    @GetMapping("")
+    public ResponseEntity<ProductListResponse> getProducts(
+            @RequestParam("page")     int page,
+            @RequestParam("limit")    int limit
+    ) {
+        // Tạo Pageable từ thông tin trang và giới hạn
+        PageRequest pageRequest = PageRequest.of(
+                page, limit,
+                Sort.by("createdAt").descending());
+        Page<ProductResponse> productPage = productService.getAllProducts(pageRequest);
+        // Lấy tổng số trang
+        int totalPages = productPage.getTotalPages();
+        List<ProductResponse> products = productPage.getContent();
+        return ResponseEntity.ok(ProductListResponse
+                .builder()
+                .products(products)
+                .totalPages(totalPages)
+                .build());
+    }
+    //http://localhost:8088/api/v1/products/6
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(
+            @PathVariable("id") Long productId
+    ) {
+        try {
+            Product existingProduct = productService.getProductById(productId);
+            return ResponseEntity.ok(ProductResponse.fromProduct(existingProduct));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
 
+    }
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
-        return ResponseEntity.ok("deleted " + id);
+    public ResponseEntity<String> deleteProduct(@PathVariable long id) {
+        try {
+            productService.deleteProduct(id);
+            return ResponseEntity.ok(String.format("Product with id = %d deleted successfully", id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    //update a product
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProduct(
+            @PathVariable long id,
+            @RequestBody ProductDTO productDTO) {
+        try {
+            Product updatedProduct = productService.updateProduct(id, productDTO);
+            return ResponseEntity.ok(updatedProduct);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 }
---
+++ shopapp/src/main/java/com/project/shopapp/services/ProductService.java
@@ -0,0 +1,119 @@
+package com.project.shopapp.services;
+
+import com.project.shopapp.dto.ProductDTO;
+import com.project.shopapp.dto.ProductImageDTO;
+import com.project.shopapp.model.Category;
+import com.project.shopapp.model.Product;
+import com.project.shopapp.model.ProductImage;
+import com.project.shopapp.repositories.CategoryRepository;
+import com.project.shopapp.repositories.ProductImageRepository;
+import com.project.shopapp.repositories.ProductRepository;
+import com.project.shopapp.responses.ProductResponse;
+import com.project.shopapp.services.interfaces.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService implements IProductService {
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final ProductImageRepository productImageRepository;
+    @Override
+    public Product createProduct(ProductDTO productDTO) throws Exception {
+        Category existingCategory = categoryRepository
+                .findById(productDTO.getCategoryId())
+                .orElseThrow(() ->
+                        new Exception(
+                                "Cannot find : "+productDTO.getCategoryId()));
+
+        Product newProduct = Product.builder()
+                .name(productDTO.getName())
+                .price(productDTO.getPrice())
+                .thumbnail(productDTO.getThumbnail())
+                .description(productDTO.getDescription())
+                .category(existingCategory)
+                .build();
+        return productRepository.save(newProduct);
+    }
+
+    @Override
+    public Product getProductById(long productId) throws Exception {
+        return productRepository.findById(productId).
+                orElseThrow(()-> new Exception(
+                        "Cannot find "+productId));
+    }
+
+    @Override
+    public Page<ProductResponse> getAllProducts(PageRequest pageRequest) {
+//        return productRepository
+//                .findAll(pageRequest)
+//                .map(product -> ProductResponse.fromProduct(product));
+        return productRepository    // cái này đẹp hơn, được IDE khuyến khích
+                .findAll(pageRequest)
+                .map(ProductResponse::fromProduct);
+
+    }
+
+    @Override
+    public Product updateProduct(
+            long id,
+            ProductDTO productDTO
+    )
+            throws Exception {
+        Product existingProduct = getProductById(id);
+        if(existingProduct != null) {
+            //copy các thuộc tính từ DTO -> Product
+            //Có thể sử dụng ModelMapper
+            Category existingCategory = categoryRepository
+                    .findById(productDTO.getCategoryId())
+                    .orElseThrow(() ->
+                            new Exception(
+                                    "Cannot find "+productDTO.getCategoryId()));
+            existingProduct.setName(productDTO.getName());
+            existingProduct.setCategory(existingCategory);
+            existingProduct.setPrice(productDTO.getPrice());
+            existingProduct.setDescription(productDTO.getDescription());
+            existingProduct.setThumbnail(productDTO.getThumbnail());
+            return productRepository.save(existingProduct);
+        }
+        return null;
+
+    }
+
+    @Override
+    public void deleteProduct(long id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        optionalProduct.ifPresent(productRepository::delete);
+    }
+
+    @Override
+    public boolean existsByName(String name) {
+        return productRepository.existsByName(name);
+    }
+    @Override
+    public ProductImage createProductImage(
+            Long productId,
+            ProductImageDTO productImageDTO) throws Exception {
+        Product existingProduct = productRepository
+                .findById(productId)
+                .orElseThrow(() ->
+                        new Exception(
+                                "Cannot find: "+productImageDTO.getProductId()));
+        ProductImage newProductImage = ProductImage.builder()
+                .product(existingProduct)
+                .imageUrl(productImageDTO.getImageUrl())
+                .build();
+        int size = productImageRepository.findByProductId(productId).size();
+        if(size >= ProductImage.MAXIMUM_IMAGES_PER_PRODUCT) {
+            throw new Exception(
+                    "quantity of images <= "
+                    +ProductImage.MAXIMUM_IMAGES_PER_PRODUCT);
+        }
+        return productImageRepository.save(newProductImage);
+    }
+}
