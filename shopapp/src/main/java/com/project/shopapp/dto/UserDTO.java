5a869da9138aae0b1ede938c38be9cf6bd608287
Nguyễn Thái Vinh <VinhNT@gmail.com>
2024-08-20 00:41:39 +0700

    update User
    User DTO, User entity,

--- shopapp/src/main/java/com/project/shopapp/dto/UserDTO.java
+++ shopapp/src/main/java/com/project/shopapp/dto/UserDTO.java
@@ -2,38 +2,43 @@ package com.project.shopapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
-import org.springframework.validation.annotation.Validated;
 
 import java.util.Date;
 
-@Builder
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Validated
+@Getter
+@Setter
 public class UserDTO {
-    @JsonProperty("full_name")
+    @JsonProperty("fullname")
     private String fullName;
 
     @JsonProperty("phone_number")
-    @NotBlank(message = "cant be null")
+    @NotBlank(message = "Phone number is required")
     private String phoneNumber;
 
     private String address;
 
-    @NotBlank(message = "cant be null")
+    @NotBlank(message = "Password cannot be blank")
     private String password;
 
-    private String repeatPassword;
+    @JsonProperty("retype_password")
+    private String retypePassword;
 
-    @JsonProperty("date_birth")
-    private Date dateBirth;
+    @JsonProperty("date_of_birth")
+    private Date dateOfBirth;
+
     @JsonProperty("facebook_account_id")
     private int facebookAccountId;
+
     @JsonProperty("google_account_id")
     private int googleAccountId;
+
+    @NotNull(message = "Role ID is required")
     @JsonProperty("role_id")
     private Long roleId;
-
 }
--- shopapp/src/main/java/com/project/shopapp/dto/UserLoginDTO.java
+++ shopapp/src/main/java/com/project/shopapp/dto/UserLoginDTO.java
@@ -1,20 +1,26 @@
 package com.project.shopapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Validated
 public class UserLoginDTO {
     @JsonProperty("phone_number")
+    @NotBlank(message = "Phone number is required")
     private String phoneNumber;
 
+    @NotBlank(message = "Password cannot be blank")
     private String password;
+
+    @Min(value = 1, message = "You must enter role's Id")
+    @JsonProperty("role_id")
+    private Long roleId;
 }
