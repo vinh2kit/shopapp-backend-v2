2f5b68f28d9da5f0d70edd9b6bbfb3a7f5978458
Nguyễn Thái Vinh <VinhNT@gmail.com>
2024-08-28 01:11:50 +0700

    add Elasticsearch on products
    Don't use the Elasticsearch library
    Just demo, using native queries (MATCH AGAINST)

--- shopapp/src/main/java/com/project/shopapp/repositories/ProductRepository.java
+++ shopapp/src/main/java/com/project/shopapp/repositories/ProductRepository.java
@@ -7,4 +7,12 @@ import org.springframework.data.domain.*;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByName(String name);
     Page<Product> findAll(Pageable pageable);//phân trang
+
+    @Query(value = "SELECT * FROM products WHERE enabled = true AND "
+            + "MATCH(name, description) AGAINST (:keyword) "
+            + "AND category_id = :categoryId",
+            nativeQuery = true)
+    Page<Product> searchProducts(
+            @Param("categoryId") Long categoryId,
+            @Param("keyword") String keyword, Pageable pageable);
 }
